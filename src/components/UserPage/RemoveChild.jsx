e498cf530bc3d386e81e4f396cca9b2e204256e6
Ryan Hess <ryanmhess@gmail.com>
2022-10-10 10:52:29 -0500

    updated/created components

---
+++ src/components/UserPage/RemoveChild.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+//  function that handles the removal of child account from db
+function RemoveChild({ child }) {
+
+    const userId = useSelector((store) => store.user.id);
+    const dispatch = useDispatch();
+
+    const removeChild = () => {
+        console.log(`This button will remove ${child.username} with ID: ${child.id}.`);
+        const childIdToRemove = child.id;
+        dispatch({
+            type: 'REMOVE_CHILD',
+            payload: {childIdToRemove, userId}
+        });
+    }
+
+    return (
+        <>
+            <td>{child.username}</td>
+            <td>
+                <button onClick={removeChild}>Remove</button>
+            </td>
+        </>
+    )
+}
+
+export default RemoveChild;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import RemoveChild from './RemoveChild';
+import RegisterChild from './RegisterChild';
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
-  return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-    </div>
-  );
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch({
+			type: 'FETCH_CHILDREN',
+			payload: userId,
+		})
+	}, []);
+	const user = useSelector((store) => store.user);
+	const userId = useSelector((store) => store.user.id);
+	const children = useSelector((store) => store.children);
+	return (
+		<div className="container">
+			<h2>Welcome, {user.username}!</h2>
+			<p>Your ID is: {user.id}</p>
+			<p>
+                Account Type: {user.is_parent ? "Parent" : "Child"}
+            </p>
+			<div className="childContainer">
+                {user.is_parent ? 
+                    <table className="childItem">
+                        <thead>
+                            <tr>
+                                <th>Children</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {children.map(child => (
+                            <tr key={child.id}>
+                                <RemoveChild child={child}/>
+                            </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                : ""}    
+                {user.is_parent ? 
+                    <RegisterChild />
+                : ""}  
+            </div>
+			<LogOutButton className="btn" />
+		</div>
+	);
 }
 
 // this allows us to use <App /> in index.js
