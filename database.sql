ffc891becff85725a6f8dd868ae6abbecb1890d5
Ryan Hess <ryanmhess@gmail.com>
2022-10-10 11:02:27 -0500

    updated database.sql - created database - updated database name in pool.js

--- database.sql
+++ database.sql
@@ -1,10 +1,17 @@
+--DROP TABLE "user";
 
+
+-- Database name = quest_board
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "is_parent" BOOLEAN DEFAULT TRUE,
+    "parent_id" INT DEFAULT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "code" VARCHAR(4) DEFAULT NULL
 );
